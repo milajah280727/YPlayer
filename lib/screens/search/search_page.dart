75d57e19555f45d2b5b99e5a42222f38aee14b17
milajah280727 <demaloncom812@gmail.com>
2025-11-27 12:47:44 +0700

    menambahkan fitur search dan koneksi ke api youtube

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -5,23 +5,42 @@ import 'package:yplayer/main.dart';
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
+
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.white,
         backgroundColor: Colors.pink,
+        automaticallyImplyLeading: false,
         leading: IconButton(onPressed: (){
           Navigator.pop(context);
           Navigator.push(context, MaterialPageRoute(builder: (context) => HalamanUtama()));
         }, icon: Icon(Icons.arrow_back)),
-        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.search))],
+        actions: [IconButton(onPressed: (){
+        }, icon: Icon(Icons.search))],
+        title: TextField(
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.white
+          ),
+          decoration: InputDecoration(
+            hintText: "Cari disini",
+            hintStyle: TextStyle(
+              fontSize: 20,
+              color: Colors.white
+            ),
+            border: InputBorder.none
+          ),
+        ),
       ),
+      body: Padding(padding: EdgeInsetsGeometry.fromLTRB(0, 0, 0, 1)),
     );
   }
 }
